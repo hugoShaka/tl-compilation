31e42af7458a73bf6a312b4f75482db31898c898
g-goessel <me+github@guimoz.fr>
2019-01-21 15:49:16 +0100

    deplacement hello.c

--- lexeur/hello.d
+++ lexeur/hello.d
@@ -1,7 +1,6 @@
-#include "string.h"
-#include "library.h"
-
 uint64_t main(uint64_t argc, uint64_t *argv){
   init_library();
   printf1("%s\n", "Hello World");
 }
+#include "string.h"
+#include "library.h"
